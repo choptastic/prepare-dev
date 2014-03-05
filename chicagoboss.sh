db004761775851ae8030d02aa6b244145362bb3f
Jesse Gumm <sigmastar@gmail.com>
2014-03-05 15:05:21 -0600

    Add chicagoboss cloning

---
+++ chicagoboss.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mkdir -p /www/chicagoboss-git
+
+cd /www/chicagoboss-git
+git clone git@github.com:choptastic/chicagoboss
+git clone git@github.com:choptastic/chicagoboss.org
+git clone git@github.com:choptastic/boss_db
+git clone git@github.com:choptastic/boss_mq
+git clone git@github.com:choptastic/aleppo
+git clone git@github.com:choptastic/mochicow
+git clone git@github.com:choptastic/mongodb-erlang
+git clone git@github.com:choptastic/tiny_pq
+git clone git@github.com:choptastic/tinymq

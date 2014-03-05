d953dd6c812314836e452dd0a07643b2607fcd1c
Jesse Gumm <sigmastar@gmail.com>
2014-03-05 15:05:08 -0600

    Add nitrogen to initialization script

---
+++ nitrogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+mkdir -p /www/nitrogen-git
+cd /www/nitrogen-git
+
+git clone git@github.com:choptastic/nitrogen
+git clone git@github.com:choptastic/nprocreg
+git clone git@github.com:choptastic/simple_bridge
+git clone git@github.com:choptastic/NitrogenProject.com
+git clone git@github.com:choptastic/sample_nitrogen_plugin
+git clone git@github.com:choptastic/nitrogen_core
+
